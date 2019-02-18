ccee7ca6b6a59a8a639499fc60dfdcc1d6cdb4f1
ramon <ramonmorcillocascales@gmail.com>
2019-02-18 08:56:28 -0800

    part4 done, sprite scale , rotating, color,... float important

--- tutorial_project_1/src/game.cpp
+++ tutorial_project_1/src/game.cpp
@@ -24,8 +24,18 @@ game::game(int width, int height, std::string title)
     // Now we will set the second sprite in a diferent position
     spr2->setPosition(400,300);
     // We are going to set the origin of the sprite in the middle not in the up-left corner
-    spr2->setOrigin(spr2->getTexture()->getSize().x / 2.f, spr2->getTexture()->getSize().y / 2.f);
+    spr2->setOrigin((float)(spr2->getTexture()->getSize().x / 2), spr2->getTexture()->getSize().y / 2.f);
 
+    // Change the color
+    spr2->setColor(Color::Red);
+
+    // Scale the size
+    spr2->setScale(100.f/spr2->getTexture()->getSize().x, 50.f/spr2->getTexture()->getSize().y); // desired size / actual size
+
+    // Scale to full window size
+    spr1->setScale(((float)window1->getSize().x/(float)spr1->getTexture()->getSize().x), ((float)window1->getSize().y/(float)spr1->getTexture()->getSize().y));
+
+    // Rotate 90 degrees
     spr2->setRotation(90);
 
     gameLoop();
