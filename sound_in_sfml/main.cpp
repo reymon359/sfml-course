709dce6ed1355d3db2d41ce3c68ce1c3288bf3d5
ramon <ramonmorcillocascales@gmail.com>
2019-02-25 08:14:48 -0800

    sfml sound tutorial part 2 it reproduces sound when click. well, it should

--- sound_in_sfml/main.cpp
+++ sound_in_sfml/main.cpp
@@ -6,12 +6,54 @@ using namespace sf;
 using namespace std;
 
 int main()
-    RenderWindow window(VideoMode(800,600). "sound tutorial");
+{
+    RenderWindow window(VideoMode(800,600),"sound tutorial");
 
     // the soundbuffer has the sound file (like the texture)
     SoundBuffer buffer;
     // The sound loads the buffer (like the sprite loads the texture)
     Sound sound;
 
-{
+    if(!buffer.loadFromFile("1.wav"))
+    {
+        cout<<"Could not be loaded"<<endl;
+
+    }
+    // Now we have the sound and can work with it
+    sound.setBuffer(buffer);
+
+    while(window.isOpen())
+    {
+
+        // Controll events
+        Event event;
+
+        while (window.pollEvent(event))
+        {
+            // CLose window
+            if(event.type == Event::Closed)
+            {
+                window.close();
+            }
+
+            // Mouse button makes a sound
+            if(event.type==Event::MouseButtonPressed)
+            {
+                if(event.mouseButton.button == Mouse::Button::Left)
+                {
+                sound.setVolume(65);
+                    cout<<"sound 1";
+                    sound.play();
+                }
+
+
+            }
+
+
+        }
+        window.clear();
+        window.display();
+    }
+    return 0;
+}
 
